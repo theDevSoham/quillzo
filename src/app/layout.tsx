131c3ec237328d45235ba10c4cbb7397cce39e32
Soham Das <publicsoham.24@gmail.com>
2024-09-18 16:01:08 +0530

    feat: added google and github auth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import RecoilContextProvider from "@/lib/RecoilContextProvider";
+import SessionWrapper from "@/lib/SessionWrapper";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,10 +17,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${inter.className} antialiased`}>
-        <RecoilContextProvider>{children}</RecoilContextProvider>
-      </body>
-    </html>
+    <SessionWrapper>
+      <html lang="en">
+        <body className={`${inter.className} antialiased`}>
+          <RecoilContextProvider>{children}</RecoilContextProvider>
+        </body>
+      </html>
+    </SessionWrapper>
   );
 }
