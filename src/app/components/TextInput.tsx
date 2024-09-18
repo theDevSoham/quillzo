663851a708d354d5c39499686ec8a3459809b01f
Soham Das <publicsoham.24@gmail.com>
2024-09-18 12:19:48 +0530

    feat: added recoil and made font changes

---
+++ src/app/components/TextInput.tsx
@@ -0,0 +1,18 @@
+"use client"
+
+import { useRecoilState } from "recoil";
+import { textState } from "@/state/state";
+function TextInput() {
+  const [text, setText] = useRecoilState(textState);
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setText(event.target.value);
+  };
+  return (
+    <div>
+      <input type="text" value={text} onChange={onChange} />
+      <br />
+      Echo: {text}
+    </div>
+  );
+}
+export default TextInput;
