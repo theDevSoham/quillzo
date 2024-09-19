cf58b3af36e0b9351a505ef02f90caef8c641627
Soham Das <publicsoham.24@gmail.com>
2024-09-19 21:55:27 +0530

    feat: added navbar animation

--- src/components/custom/NavBar.tsx
+++ src/components/custom/NavBar.tsx
@@ -1,3 +1,4 @@
+"use client";
 import {
   Sheet,
   SheetTrigger,
@@ -10,97 +11,122 @@ import { ThemeSwitch } from "../theme-switch";
 import Image from "next/image";
 import logo from "@/public/images/logo.webp";
 import { playpenSans } from "@/fonts/fonts";
+import { useEffect, useState } from "react";
+
+const scrolledClasses =
+  "p-1 bg-background/50 sticky top-0 backdrop-blur border-b z-10";
+const unscrolledClasses = "p-3 flex border-b";
 
 export default function NavBar() {
-  return (
-    <header className="flex justify-between items-center lg:flex-row flex-row-reverse h-20 w-full shrink-0 items-center px-4 md:px-6">
-      <Sheet>
-        <SheetTrigger asChild>
-          <Button variant="outline" size="icon" className="lg:hidden">
-            <MenuIcon className="h-6 w-6" />
-          </Button>
-        </SheetTrigger>
-        <SheetTitle className="flex md:hidden font-bold">Quillzo</SheetTitle>
-        <SheetContent side="right">
-          <Link href="#" className="mr-6 hidden lg:flex" prefetch={false}>
-            <MountainIcon className="h-[50px] w-[50px]" />
-            <span className="sr-only">Quillzo</span>
-          </Link>
-          <div className="grid gap-2 py-6">
-            <Link
-              href="#"
-              className="flex w-full items-center py-2 text-lg font-semibold"
-              prefetch={false}
-            >
-              Home
-            </Link>
-            <Link
-              href="#"
-              className="flex w-full items-center py-2 text-lg font-semibold"
-              prefetch={false}
-            >
-              About
-            </Link>
-            <Link
-              href="#"
-              className="flex w-full items-center py-2 text-lg font-semibold"
-              prefetch={false}
-            >
-              Services
-            </Link>
-            <Link
-              href="#"
-              className="flex w-full items-center py-2 text-lg font-semibold"
-              prefetch={false}
-            >
-              Contact
-            </Link>
-            <ThemeSwitch />
-          </div>
-        </SheetContent>
-      </Sheet>
-      <Link href="/" className="mr-6 flex" prefetch={false}>
-        <div className="flex justify-center items-center gap-5">
-          <MountainIcon className="lg:h-[50px] md:h-[40px] lg:w-[50px] md:w-[40px] h-[55px] w-[55px]" />
-          <span
-            className={`hidden md:flex ${playpenSans.className} font-bold lg:text-2xl md:text-xl`}
-          >
-            Quillzo
-          </span>
-        </div>
-      </Link>
-      <nav className="ml-auto hidden lg:flex gap-6">
-        <Link
-          href="#"
-          className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50"
-          prefetch={false}
-        >
-          Home
-        </Link>
-        <Link
-          href="#"
-          className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50"
-          prefetch={false}
-        >
-          About
-        </Link>
-        <Link
-          href="#"
-          className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50"
-          prefetch={false}
-        >
-          Services
-        </Link>
-        <Link
-          href="#"
-          className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50"
-          prefetch={false}
-        >
-          Contact
-        </Link>
+  const [scrolled, setScrolled] = useState<boolean>(false);
 
-        <ThemeSwitch />
-      </nav>
+  const scrollEffect = () => {
+    if (window.scrollY >= 30) {
+      setScrolled(true);
+    } else {
+      setScrolled(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", scrollEffect);
+
+    return () => {
+      window.removeEventListener("scroll", scrollEffect);
+    };
+  }, []);
+
+  return (
+    <header className={`${scrolled ? scrolledClasses : unscrolledClasses}`}>
+      <div className="w-full flex justify-between items-center lg:flex-row flex-row-reverse h-20 w-full shrink-0 items-center px-4 md:px-6">
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button variant="outline" size="icon" className="lg:hidden">
+              <MenuIcon className="h-6 w-6" />
+            </Button>
+          </SheetTrigger>
+          <SheetTitle className="flex md:hidden font-bold">Quillzo</SheetTitle>
+          <SheetContent side="right">
+            <Link href="#" className="mr-6 hidden lg:flex" prefetch={false}>
+              <MountainIcon className="h-[50px] w-[50px]" />
+              <span className="sr-only">Quillzo</span>
+            </Link>
+            <div className="grid gap-2 py-6">
+              <Link
+                href="#"
+                className="flex w-full items-center py-2 text-lg font-semibold"
+                prefetch={false}
+              >
+                Home
+              </Link>
+              <Link
+                href="#"
+                className="flex w-full items-center py-2 text-lg font-semibold"
+                prefetch={false}
+              >
+                About
+              </Link>
+              <Link
+                href="#"
+                className="flex w-full items-center py-2 text-lg font-semibold"
+                prefetch={false}
+              >
+                Services
+              </Link>
+              <Link
+                href="#"
+                className="flex w-full items-center py-2 text-lg font-semibold"
+                prefetch={false}
+              >
+                Contact
+              </Link>
+              <ThemeSwitch />
+            </div>
+          </SheetContent>
+        </Sheet>
+        <Link href="/" className="mr-6 flex" prefetch={false}>
+          <div className="flex justify-center items-center gap-5">
+            <MountainIcon className="lg:h-[50px] md:h-[40px] lg:w-[50px] md:w-[40px] h-[55px] w-[55px]" />
+            <span
+              className={`hidden md:flex ${playpenSans.className} font-bold lg:text-2xl md:text-xl`}
+            >
+              Quillzo
+            </span>
+          </div>
+        </Link>
+        <nav className="ml-auto hidden lg:flex gap-6">
+          <Link
+            href="#"
+            className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50"
+            prefetch={false}
+          >
+            Home
+          </Link>
+          <Link
+            href="#"
+            className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50"
+            prefetch={false}
+          >
+            About
+          </Link>
+          <Link
+            href="#"
+            className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50"
+            prefetch={false}
+          >
+            Services
+          </Link>
+          <Link
+            href="#"
+            className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50"
+            prefetch={false}
+          >
+            Contact
+          </Link>
+
+          <ThemeSwitch />
+        </nav>
+      </div>
     </header>
   );
 }
